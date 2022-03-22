df61f8cb44686dda2a425e7f968359b8b3d9f023
Y.Arimitsu <yarimit@gmail.com>
2022-03-22 21:33:06 +0900

    Fix DtaaSource

--- UseCommonModel/Program.cs
+++ UseCommonModel/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using CommonModelLib.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
 
 namespace UseCommonModel
 {
     internal class Program
     {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddDbContext<CommonDbContext>(options =>
+                    {
+                        options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=SampleDB;Trusted_Connection=True;");
+                    });
+                    services.AddSingleton<MyService>();
+                });
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateHostBuilder(args).Build();
+            var myService = host.Services.GetRequiredService<MyService>();
+            myService.DoSomething();
+        }
+    }
+
+    public class MyService
+    {
+        private readonly CommonDbContext _CommonDbContext;
+        public MyService(CommonDbContext dbContext)
+        {
+            _CommonDbContext = dbContext;
+        }
+
+        public void DoSomething()
+        {
+            var blogs = _CommonDbContext.Blogs.ToList();
+            foreach(var blog in blogs)
+            {
+                Console.WriteLine($"Blog:{blog.Url}");
+            }
+
         }
     }
 }
